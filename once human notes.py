217043d8452a79c547eff5f3f59710c3605d097b
300AB <300AB@users.noreply.github.com>
2025-06-14 20:11:51 -0500

    init

---
+++ once human notes.py
@@ -0,0 +1,279 @@
+#------------------------------------------------------------
+
+
+
+#------------------------------------------------------------
+
+#Deviations missing
+
+
+# Pup Buddy: [Pvp only] steak kill zombie w/ loon , crates, 
+
+# Masonic Pyamid: [Pvp only] Prime War
+
+
+
+# Mr.Wish: Sigma Silo
+# Dr.Teddy: [Pvp Only] Theta Silo
+# Upper World Spawn:[Pvp only] PSI Silo
+
+
+#------------------------------------------------------------
+
+# pro / chests: alpha silo 13 min
+# pro w/o chests: alpha silo 11 min
+
+# hard w/ ish chests: alpha silo 9min messing around
+# pro w/ chests: alpha silo 11 min pistol n' ar
+
+# pro / chests: theta silo 15 min
+# pro w/o chests: theta silo 9 min
+
+# 
+# 
+
+#------------------------------------------------------------
+
+# I  -  6 
+# II - 4 + 2
+# III- 7 + 5
+# IV - 14 + 7
+
+# Tree 1.57 min
+# Dog 2.32 min
+# Spider 3.00 min
+# Mini-Gun 6.09 min
+
+#------------------------------------------------------------
+
+# Tier: I
+# Lvl 1 Req 
+
+# Tier: II
+# Lvl 10 Req
+
+# Tier: III
+# Lvl 20 Req
+
+# Tier: IV
+# Lvl 30 Req
+
+# Tier: V
+# Lvl 40 Req
+
+#------------------------------------------------------------
+
+# ALPHA Lvl: 35 Head
+    # Electric gen boss + shield unit boss [electric]
+        # Div
+        # pro: 7:17-9:46
+        # hard: 
+        # H37: robot gatherer, [secret boss]
+
+# EX1   Lvl: 35 Mask
+    # Stoplight + no side boss? [none but shrap ez mode]
+        # Div
+        # pro:
+        # hard: 5:00-11:22
+        # Lighter: nade crafter, [main boss]
+
+# THETA Lvl: 45 Chest
+    # Reflection statue boss + Spore break boss [none]
+        # Div
+        # pro: 
+        # hard: 
+        # Enchanting Void: melee buff cat, [secret boss]
+
+# SIGMA Lvl: 15 Gloves
+    # Mr.Wish + Immune Minigun Rosetta [none]
+        # Div
+        # pro: 
+        # hard: 
+        # Mr.Wish: bullseye spread, [secret boss]
+        # Leathel Rabbit: harvest stuff [secret boss]
+
+# PSI   Lvl: 45 Pants
+    # Fridge boss + unknown if side boss [blast]
+        # Rosesetta
+        # pro: 15:00
+        # hard: 7:37-9:00
+        # Snowglobe: nade crafting snowballs, [main boss]
+        # World Spawn: potion, [main boss pvp only]
+
+# PHI   Lvl: 25 Boots
+    # Mother gel boss + Spawn sack boss [fire]
+        # Div
+        # pro: 
+        # hard: 
+        # Chef & Pyro Dinos, [secret boss 2x versions]
+
+
+# Mod list left == mask | EX1
+
+#------------------------------------------------------------
+
+# 158 sorce controller + pro [secret]
+# 178 sorce controller + pro [main]
+# 334 run 1 pro aprox
+
+#------------------------------------------------------------
+
+# Net Tier Gold V = 6,600 Links, 45 Bp Fabric / SpPlastic, 490 Sorce
+# Net Tier Purple V 0-6 Cali Armor Sorce = 417 Sorce
+# Net Tier Green V 0-6 Cali Armor Sorce = 394 Sorce
+
+
+# Tier V Gold Cali Armor 0-6:
+
+# Tier 1/6 = 10 Bulletproof Fiber Fabric, 400 Links
+# 35 24 Tier 2/6 = 10 Special Plastic, 40 Sorce, 800 Links
+# 51 36 Tier 3/6 = 15 Bulletproof Fiber Fabric, 60 Sorce, 1200 Links
+# 76 54 Tier 4/6 = 15 Special Plastic, 90 Sorce, 1600 Links
+# 102 72 Tier 5/6 = 20 Bulletproof Fiber Fabric, 120 Sorce, 2000 Links
+# 153 108 Tier 6/6 = 20 Special Plastic, 180 Sorce, 2400 Links
+
+#------------------------------------------------------------
+
+# Tier V Gold Cali Weapon 0-10:
+
+# Net = 44,800 Links, 535 Tungsten, 40 Play cry, 3,080 Sorce
+
+# 10 Tungsten, 800 Links
+# 15 Tungsten, 1200 Links
+# 20 Tungsten, 60 Sorce, 1600 Links
+# 30 Tungsten, 120 Sorce, 1400 Links
+# 50 Tungsten 4 Plat cry, 200 Sorce, 3200 Links
+# 50 Tungsten, 6 Plat cry, 300 Sorce, 4000 Links
+# 80 Tungsten, 10 Plat cry, 500 Sorce, 6000 Links
+# 80 Tungsten, 4 Plat cry, 500 Sorce, 7000 Links
+# 100 Tungsten, 6 Plat Cry, 600 Sorce, 8000 Links
+# 100 Tungsten, 10 Plat cry, 800 Sorce, 10000 Links
+
+#------------------------------------------------------------
+
+
+# 13 runs of a duo silo
+# 4,080
+
+# 2,062 = 4 purp 1 green cali to 6 in sorce
+
+# 250,000 = full map market 50k per town, 5 towns
+#   Gold bar = 12,000?
+#   Silver bar = 6,000
+#   Rare Crystal Set = 12,000
+
+# 41.6 Silver bars needed to max
+
+# 8 Silver per vender = 48k
+# 240,000 = 40 silver full map 2k left per vender
+
+#------------------------------------------------------------
+
+# Formulas Needed
+
+# Standing Searchlight [Blue]
+# Sofa Chair [Blue]
+# 
+
+
+# Low Cupboard [Blue]
+# Industrial Coffee Table [Blue]
+# Industrial a Cabinet [Blue]
+# Industrial b Cabinet [Blue]
+
+# Amarican Bed [Purple]
+# Amarican Chair [Purple]
+# Amarican Sofa I [Purple]
+# Amarican Sofa II [Purple]
+# Amarican Sofa III [Purple]
+
+
+#------------------------------------------------------------
+
+# Transfer List
+
+# Divs 5 x 800 = 4000 * 2 = 8k
+# 1 Silver
+# 2 Moonlit
+# 3 Frog
+# 4 Chef
+# 5 Hand
+# 6 Twister
+# 7 H37
+# 8 Dig
+# 9 Log
+# 10 Doll
+
+#------------------------------------------------------------
+
+# Purple Diamond Locations
+
+# [Dayton Wetlands:(6620, -5414)] - not bp
+# [Dayton Wetlands:(5620, -5212)] - not bp
+
+# [Broken Delta:(5961, -5984)] - not bp
+# [Broken Delta:(6061, -6933)] - not bp
+# [Broken Delta:(7433, -7247)] - not bp
+# [Broken Delta:(4565, -5221)] - not bp
+
+# [Iron River:(5151, -3637)] - mebe bp
+# [Iron River:(5622, -3210)] - tumbs up bp
+# [Iron River:(6090, -3041)] - mebe bp
+# [Iron River:(6558, -3421)] - mebe bp
+
+# [Chalk Peak:(3744, -4493)] - statue
+# [Chalk Peak:(2417, -4068)] - mebe bp
+# [Chalk Peak:(2724, -5372)] - arrow bp
+# [Chalk Peak:(1868, -5781)] - arrow bp 
+
+# [Red Sands:(2762, -1626)] - statue
+# [Red Sands:(4499, -2302)] - statue
+# [Red Sands:(5148, -1361)] - statue
+# [Red Sands:(6116, -1077)] - statue
+
+# [Blackheart Region:(3505, -5051)] - mebe bp
+# [Blackheart Region:(2507, -44)] - statue
+# [:(-16014, 556)] ?????? not in prism
+# [Blackheart Region:(5240, 1515)] ???? not in prism
+
+#------------------------------------------------------------
+
+# Shop Math
+
+# 80 = 50%0ff
+# 160 = normal
+
+# 10  = 1600
+# 10  = 800
+
+#------------------------------------------------------------
+
+# Blueprint Math
+
+#head
+# hp 590 -> 738 [148]
+# psi 92 -> 115 [23]
+
+#mask
+# hp 295 -> 369 [74]
+# psi 115 -> 144 [29]
+
+#chest
+# hp 1534 -> 1918 [384]
+# psi 64 -> 80 [16]
+
+#gloves
+# hp 885 -> 1106 [221]
+# psi 69 -> 86 [17]
+
+#boots
+# hp 1180 -> 1475 [295]
+# psi 46 -> 58 [12]
+
+#------------------------------------------------------------
+
+# psi == ice pot
+
+
+
+
---
+++ poe-notes.py
@@ -0,0 +1,37 @@
+
+
+110 #misery shield sharge effect
+
+140 #celectial brand
+
+
+
+
+
+
+180 #siren weapon effect
+
+210 #siren char effect
+
+
+
+40 #stash delve x
+
+
+
+140 #collection stash tab
+
+other = 50+100
+
+stash = 40*3+50+140
+stash_lite = 40*3+50
+
+siren = 100+125+180*2+210
+siren_lite = 100
+
+lite = siren_lite+stash_lite
+
+print(stash_lite+other)
+
+
+
